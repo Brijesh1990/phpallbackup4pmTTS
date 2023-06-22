00f5da9562525379f254d36e87daf4bb63742572
Brijesh1990 <bkpandey.pandey@gmail.com>
2023-06-22 16:33:53 +0530

    all data updated

---
+++ module5/nasha-mukti-app/404.php
@@ -0,0 +1,25 @@
+
+
+  <main id="main">
+    <!-- ======= Breadcrumbs Section ======= -->
+    <section class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>Home/404 page not found</h2>
+          <ol>
+            <li><a href="index.html">Home</a></li>
+            <li>404 page not found</li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Breadcrumbs Section -->
+
+    <section class="inner-page">
+      <div class="container">
+        <p> <img src="assets/img/404/404.gif"> </p>
+      </div>
+    </section>
+
+  </main><!-- End #main -->
---
+++ module5/nasha-mukti-app/controller/controller.php
@@ -0,0 +1,41 @@
+<?php 
+require_once("model/model.php");
+class controller extends model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        // view template create routing
+        if(isset($_SERVER["PATH_INFO"]))
+        {
+            switch($_SERVER["PATH_INFO"])
+            {
+                case '/':
+                    require_once("index.php");
+                    require_once("header.php");
+                    require_once("navbar.php");
+                    require_once("banner.php");
+                    require_once("content.php");
+                    require_once("footer.php");
+                    break;
+
+                default:
+                {
+
+                    require_once("index.php");
+                    require_once("header.php");
+                    require_once("navbar.php");
+                    require_once("404.php");
+                    require_once("footer.php");
+                    break;
+                }    
+            }
+
+        }
+   
+
+    }
+}
+$obj=new controller;
+
+?>
